bd1653888d5ef5677a18d1d6476b71a040c81657
Squarely <pb@squarely.net>
2026-07-10 22:39:16 -0700

    Prepare Clench Wallet 0.3.20 release candidate

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "net.clench.wallet"
         minSdk = 26
         targetSdk = 35
-        versionCode = 319
-        versionName = "0.3.19"
+        versionCode = 320
+        versionName = "0.3.20"
 
         manifestPlaceholders["appLabel"] = "Clench"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
